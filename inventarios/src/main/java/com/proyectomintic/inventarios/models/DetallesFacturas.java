2f66893ac064afcf239ee87ab89683ddb6302669
Joseph Angel Herrera Mantilla <jahm1997@gmail.com>
2022-09-21 23:04:51 -0500

    vamos por 6

--- inventarios/src/main/java/com/proyectomintic/inventarios/models/DetallesFacturas.java
+++ inventarios/src/main/java/com/proyectomintic/inventarios/models/DetallesFacturas.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="detallesFacturas")
+@Table(name="DETALLESFACTURAS")
 public class DetallesFacturas implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
